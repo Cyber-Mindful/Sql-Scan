bc770ca6e383e4e332daaa2b7a0a9047e43c1d8e
Ahmed AL Nassif <68873513+as9697347@users.noreply.github.com>
2021-05-04 00:16:32 +0300

    Update sql-scan.py

--- sql-scan.py
+++ sql-scan.py
@@ -193,7 +193,7 @@ def start():
 					print timer+'\033[1;34m[\033[1;37m=\033[1;34m]\033[1;33m Link \033[0m\033[32m: \033[1;37m',link,'\033[0m'
 				urls.append(link)
 				time.sleep(.2)
-				if '?id=' in link or '?num=' in link or '?file=' in link or '?cat=' in link or '?ID=' in link:
+				if '?id=' in link or '%3Fid%3D' in link or '?num=' in link or '?file=' in link or '?cat=' in link or '?ID=' in link:
 					print timer+'\033[1;34m[*] \033[1;35mScan link: \033[1;36m',link,'\033[0m'
 					time.sleep(.2)
 					database.append(link)
